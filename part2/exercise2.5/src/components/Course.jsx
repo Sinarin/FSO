bb7e3a680a26b3d8a9566136db93ddbd9b3e4cc4
sinarin <alexngdragon@hotmail.com>
2025-02-21 09:01:26 +0900

    finish exercise 2.5, add functionality for handling any number of courses and parts of a course

--- part2/exercise2.5/src/components/Course.jsx
+++ part2/exercise2.5/src/components/Course.jsx
@@ -1,8 +1,17 @@
+const Total = ({total}) => {
+    return <div class="total">Total of {total} exercises</div>
+}
+
 const Course = ({course}) => {
+    const total = course.parts.reduce((sum, part) => {
+        return sum + part.exercises
+    }, 0)
+
     return (
         <div>
-            <h1>Half Stack application development</h1>
+            <h2>{course.name}</h2>
             {course.parts.map(part => <div>{part.name} {part.exercises}</div>)}
+            <Total total={total} />
         </div>
     )
 }
