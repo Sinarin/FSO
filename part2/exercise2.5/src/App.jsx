70cbd304eae47e8d31bd603e1124de5b80fcbbb1
sinarin <alexngdragon@hotmail.com>
2025-03-18 18:24:53 +0900

    exercise 2.5 finish

--- part2/exercise2.5/src/App.jsx
+++ part2/exercise2.5/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Course from  './components/Course'
+import Course from './components/Course'
 
 function App() {
   const courses = [
